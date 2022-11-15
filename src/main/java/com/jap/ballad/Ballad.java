df1556360d56357679fe5efed5945b0e44f4ddb0
ankit <ankit.kishorelv@gmail.com>
2022-11-15 21:44:19 +0530

    passed

--- src/main/java/com/jap/ballad/Ballad.java
+++ src/main/java/com/jap/ballad/Ballad.java
@@ -1,13 +1,30 @@
 package com.jap.ballad;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Ballad {
     public String readPoemFromAFile(String fileName){
        // Write the logic to read from a file
-        return "";
+        FileInputStream fileInputStream=null;
+        String str="";
+        try {
+            fileInputStream=new FileInputStream(fileName);
+
+            int value=0;
+            while((value=fileInputStream.read())!=-1){
+
+                str=str+String.valueOf((char)value);
+            }
+        }catch (FileNotFoundException e){
+            System.out.println(e.toString());
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return str;
     }
 
 
